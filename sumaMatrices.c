bea5c288d1505ccb37c6326abf28e99805934c05
Juan Antamba <juan.antamba@udla.edu.ec>
2023-11-28 14:55:05 -0500

    Se inician las bibliotecas nesesarias para los comandos a utilizar, se declaran las variables de filas y columnas, y se pide al usuario que ingrese el tamaño de las mismas

---
+++ sumaMatrices.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int filas, columnas;
+    printf("Ingrese el número de filas: ");
+    scanf("%d", &filas);
+    printf("Ingrese el número de columnas: ");
+    scanf("%d", &columnas);
+}
